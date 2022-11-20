114ce1a1fc291a20c4e6954deb98ead94d9c6958
Muhammad Mubashir Mughal <donleo26@gmail.com>
2022-11-19 23:39:55 -0500

    completed q1

--- src/Question1Tables.java
+++ src/Question1Tables.java
@@ -1,13 +1,11 @@
 public class Question1Tables {
     public static void main(String[] args)
     {
-        //int a=2;
+
         for (int a=2; a<=10; a++) {
-            for (int x=2;x<=10; x++){
-                    System.out.printf("Table of %d \n" , x);
+            System.out.printf("table of: %d  \n",a);
                 for (int i = 1; i <= 10; i++) {
                     System.out.printf(" %d * %d = %d \n", a, i, a * i);
-            }
             }
         }
     }
