a82e67ed0ddd520284541aca5cee3d8d733069c3
Muhammad Mubashir Mughal <donleo26@gmail.com>
2022-11-19 23:25:24 -0500

    nested loop for tables

--- src/Question1Tables.java
+++ src/Question1Tables.java
@@ -1,6 +1,13 @@
 public class Question1Tables {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        //int a=2;
+        for (int a=2; a<=10; a++) {
+
+            for (int i = 1; i <= 10; i++) {
+
+                System.out.printf(" %d * %d = %d \n", a, i, a * i);
+            }
+        }
     }
 }
